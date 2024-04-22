50e58a973936f384f4c50a144eecc5d449bb7d81
alsyean <alsyean@gmail.com>
2024-04-22 18:31:01 +0900

    git config test

--- lv1/running_race.js
+++ lv1/running_race.js
@@ -9,6 +9,16 @@
 선수들의 이름이 1등부터 현재 등수 순서대로 담긴 문자열 배열 players와 해설진이 부른 이름을 담은 문자열 배열 callings가 매개변수로 주어질 때,
 경주가 끝났을 때 선수들의 이름을 1등부터 등수 순서대로 배열에 담아 return 하는 solution 함수를 완성해주세요.
 
+제한사항
+5 ≤ players의 길이 ≤ 50,000
+players[i]는 i번째 선수의 이름을 의미합니다.
+players의 원소들은 알파벳 소문자로만 이루어져 있습니다.
+players에는 중복된 값이 들어가 있지 않습니다.
+3 ≤ players[i]의 길이 ≤ 10
+2 ≤ callings의 길이 ≤ 1,000,000
+callings는 players의 원소들로만 이루어져 있습니다.
+경주 진행중 1등인 선수의 이름은 불리지 않습니다.
+
  */
 
 function solution(players, callings) {
